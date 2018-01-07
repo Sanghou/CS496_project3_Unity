eecd4efa6c7583487d6beb2979c932675993e013
Lee Ju Been <ljb7977@gmail.com>
2018-01-07 22:30:32 +0900

    jump action+animation

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    private Vector2 velocity;
+
+    private float smoothTimeX=0.2f;
+    private float smoothTimeY=0.2f;
+
+    public GameObject player;
+
+    public bool bounds;
+
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+    
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y+4.22f, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+
+        if (bounds){
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public enum playerState
+    {
+        normal = 0,
+        run,
+        jump
+    }
+
+    public Animator anim;
+    public playerState state = playerState.normal;
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    public KeyCode frontKey;
+    public KeyCode backKey;
+    public KeyCode jump;
+    public KeyCode pileBlock;
+
+    private Rigidbody theRB;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
+    // Use this for initialization
+    void Start() {
+        theRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xSpeed=0, zSpeed=0;
+
+        isGrounded = Physics.Raycast(transform.position, new Vector3(0, -1), 0.5f);
+        state = playerState.normal;
+
+        if (Input.GetKey(leftKey)) {
+            xSpeed = -moveSpeed;
+            state = playerState.run;
+        } else if (Input.GetKey(rightKey)){
+            xSpeed = moveSpeed;
+            state = playerState.run;
+        }
+
+        if (Input.GetKey(frontKey))
+        {
+            zSpeed = -moveSpeed;
+            state = playerState.run;
+        }
+        else if (Input.GetKey(backKey))
+        {
+            zSpeed = moveSpeed;
+            state = playerState.run;
+        }
+
+
+        if (Input.GetKeyDown(jump) && isGrounded)
+        {
+            theRB.velocity = new Vector3(xSpeed, jumpForce, zSpeed);
+        }
+        else
+        {
+            theRB.velocity = new Vector3(xSpeed, theRB.velocity.y, zSpeed);
+        }
+
+        if (theRB.velocity.y != 0)
+            state = playerState.jump;
+
+        if (anim != null)
+        {
+            anim.SetInteger("playerState", (int)state);
+        }
+    }
+}
