c4b5ad32751774911e8937c19d6f3b6c42437407
bogo96 <bogo96@kaist.ac.kr>
2018-01-08 22:13:07 +0900

    abcd

---
+++ Assets/Script/Explosion.cs
@@ -0,0 +1,53 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public GameObject ExplosionEffect;
+    public float delay = 3f;
+    public float radius = 5f;
+    public float force = 700f;
+    float countdown;
+    bool hasExploded = false;
+
+	// Use this for initialization
+	void Start () {
+        countdown = delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+            Debug.Log("Boom!");
+        }
+	}
+
+    void Explode()
+    {
+        //show effect
+        Instantiate(ExplosionEffect, transform.position, transform.rotation);
+        //get nearby objects
+        Collider[] colliders = Physics.OverlapSphere(transform.position,radius);
+        foreach(Collider nearbyObject in colliders)
+        {
+            //add force
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+            Destructible doorbreak = nearbyObject.GetComponent<Destructible>();
+            if(doorbreak != null)
+            {
+                doorbreak.Destroy();
+            }
+        }
+        //remove bomb
+        Destroy(gameObject);
+    }
+}
