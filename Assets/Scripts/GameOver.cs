56d38f78bc5460842ff2f3b2324da9483b3e250b
bogo96 <bogo96@kaist.ac.kr>
2018-01-10 19:46:52 +0900

    merge with stage2

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("quit");
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Ending");
+        }
+    }
+
+}
